f4a9646056ddadf29c12e0c455d9c491e9562897
lampert309 <lampert309@gmail.com>
2019-01-10 09:14:13 -0800

    somthing

---
+++ SimpleMVC/Controllers/TrystinController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleMVC.Controllers
+{
+    public class TrystinController : Controller
+    {
+        public IActionResult Bio()
+        {
+            return View();
+        }
+    }
+}
